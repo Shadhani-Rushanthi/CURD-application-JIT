538915ca48997f26a71b05963568ee1769c4c84d
Shadhani Rushanthi <100184787+Shadhani-Rushanthi@users.noreply.github.com>
2023-06-23 15:52:14 +0530

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/SubjectController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using WebApplication1.Modals;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private string sqlConnectionString;
+        private readonly ILogger<SubjectController> _logger;
+
+        public SubjectController(IConfiguration configuration, ILogger<SubjectController> logger)
+        {
+            _configuration = configuration;
+            sqlConnectionString = _configuration.GetConnectionString("CURDAppDB");
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult getSubject()
+        {
+            JsonResult result = null;
+            _logger.LogInformation("get subject details get from the database");
+            try
+            {
+                string query = "select * from subject;";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                result = db.getDataSet(query);
+
+                _logger.LogInformation("get subject details result " + result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("LogError while getting subject details from the database");
+            }
+
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult getSubjectById(int id)
+        {
+            JsonResult result = null;
+            _logger.LogInformation("get Subject details by id from the database");
+            try
+            {
+                string query = "select * from subject where subject_id=" + id + ";";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                result = db.getDataSet(query);
+
+                _logger.LogInformation("get subject details by id result " + result);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("LogError while getting Subject details from the database");
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public JsonResult addSubject(Subject subject)
+        {
+            string result;
+            _logger.LogInformation("save Subject details to database");
+            try
+            {
+
+                string query = "Insert into subject values ('" + subject.subjectName + "');";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                int i = db.DataInsertUpdateDelete(query);
+                result = (i == 1) ? "Subject saved successfully" : "Failed to save Subject";
+
+                _logger.LogInformation("save subject details result " + result);
+
+            }
+            catch (Exception ex)
+            {
+                result = "Exception caused while saving Subject " + ex;
+                _logger.LogError("LogError while getting Subject details from the database");
+            }
+
+            return new JsonResult(result);
+        }
+
+        [HttpPut]
+        public JsonResult updateSubject(Subject subject)
+        {
+            string result;
+               _logger.LogInformation("update subject details get from the database");
+            try
+            {
+
+                string query = "update subject set subject_name='" + subject.subjectName + "' where subject_id=" + subject.subjectId + ";";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                int i = db.DataInsertUpdateDelete(query);
+                result = (i == 1) ? "Subject details updated successfully" : "Failed to update Subject details";
+
+                _logger.LogInformation("update subject details result " + result);
+            }
+            catch (Exception ex)
+            {
+                result = "Exception caused while updating Subject details " + ex;
+                _logger.LogError("LogError while getting Subject details from the database");
+            }
+
+            return new JsonResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult deleteSubject(int id)
+        {
+            string result;
+            _logger.LogInformation("delete Subject details get from the database");
+            try
+            {
+                string query = "delete from subject where subject_id=" + id + ";";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                int i = db.DataInsertUpdateDelete(query);
+                result = (i == 1) ? "Subject deleted successfully" : "Failed to delete Subject";
+
+                _logger.LogInformation("delete subject details result " + result);
+            }
+            catch (Exception ex)
+            {
+                result = "Exception caused while deleting Subject details " + ex;
+                _logger.LogError("LogError while getting Subject details from the database");
+            }
+
+            return new JsonResult(result);
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/TeacherController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using WebApplication1.Modals;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private string sqlConnectionString;
+        private readonly ILogger<TeacherController> _logger;
+
+        public TeacherController(IConfiguration configuration, ILogger<TeacherController> logger)
+        {
+            _configuration = configuration;
+            sqlConnectionString = _configuration.GetConnectionString("CURDAppDB");
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult getTeacher()
+        {
+            JsonResult result = null;
+            _logger.LogInformation("get teacher details get from the database");
+            try
+            {
+                string query = "select * from teacher;";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                result = db.getDataSet(query);
+
+                _logger.LogInformation("Get teacher details result " + result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("LogError while getting teacher details from the database");
+            }
+
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult getTeacherById(int id)
+        {
+            JsonResult result = null;
+            _logger.LogInformation("Teacher details get by id from the database");
+            try
+            {
+                string query = "select * from teacher where teacher_id=" + id + ";";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                result = db.getDataSet(query);
+
+                _logger.LogInformation("Get teacher details by id result " + result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("LogError while getting Teacher details by id from the database");
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public JsonResult addTeacher(Teacher teacher)
+        {
+            string result;
+            _logger.LogInformation("save teacher details get from the database");
+            try
+            {
+                string query = "Insert into teacher values ('" + teacher.firstName+ "', '"+teacher.lastName+"', "+ teacher.contactNo +", '"+ teacher.email+"');";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                int i = db.DataInsertUpdateDelete(query);
+                result = (i == 1) ? "Teacher saved successfully" : "Failed to save Teacher";
+
+                _logger.LogInformation("save teacher details result " + result);
+            }
+            catch (Exception ex)
+            {
+                result = "Exception caused while saving Teacher " + ex;
+                _logger.LogError("LogError while gettsavinging Teacher details");
+            }
+
+            return new JsonResult(result);
+        }
+
+        [HttpPut]
+        public JsonResult updateTeacher(Teacher teacher)
+        {
+            string result;
+            _logger.LogInformation("update details get from the database");
+            try
+            {
+                string query = "update teacher set first_name='" + teacher.firstName +
+                    "', last_name='" + teacher.lastName +
+                    "', contact_no=" + teacher.contactNo +
+                    ", email='" + teacher.email +
+                    "'where teacher_id=" + teacher.teacherId + ";";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                int i = db.DataInsertUpdateDelete(query);
+                result = (i == 1) ? "Teacher details updated successfully" : "Failed to update Teacher details";
+                _logger.LogInformation("update teacher details result " + result);
+            }
+            catch (Exception ex)
+            {
+                result = "Exception caused while updating Teacher details " + ex;
+                _logger.LogError("LogError while updaing Teacher details from the database");
+            }
+
+            return new JsonResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult deleteTeacher(int id)
+        {
+            string result;
+            _logger.LogInformation("delete Teacher details get from the database");
+            try
+            {
+                string query = "delete from teacher where teacher_id=" + id + ";";
+                DatabaseController db = new DatabaseController(sqlConnectionString);
+                int i = db.DataInsertUpdateDelete(query);
+                result = (i == 1) ? "Teacher deleted successfully" : "Failed to delete Teacher";
+
+                _logger.LogInformation("delete teacher details result " + result);
+            }
+            catch (Exception ex)
+            {
+                result = "Exception caused while deleting Teacher " + ex;
+                _logger.LogError("LogError while deleteing Teacher details from the database");
+            }
+
+            return new JsonResult(result);
+
+        }
+    }
+}
